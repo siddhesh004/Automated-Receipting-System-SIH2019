6b02f2d3f35c4278e926b104c573e029717cd247
Siddhesh Pai <siddhesh004@gmail.com>
2019-03-02 15:44:40 +0530

    Unzipping Added

--- Finance/pdf_extract.py
+++ Finance/pdf_extract.py
@@ -53,3 +53,28 @@ def extract_image(request):
     nlp(text)
 
 
+def extract_zip(name):
+    print(name)
+    finalpath = "templates\\Media\\" + name
+    print(finalpath)
+    raw = parser.from_file(finalpath)
+    print(raw['content'])
+    nlp(raw['content'])
+
+def extract_image_zip(name):
+    finalpath = "templates\\Media\\" + name
+    image = cv2.imread(finalpath)
+    #image = cv2.resize(image, None, fx=1, fy=1, interpolation=cv2.INTER_CUBIC)
+
+    if image is not None:
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gray = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 23, 10)
+        gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+        filename = "{}.png".format(os.getpid())
+        cv2.imwrite(filename, gray)
+        text = pytesseract.image_to_string(Image.open(filename), lang="eng")
+        os.remove(filename)
+
+        print(text)
+        nlp(text)
+
